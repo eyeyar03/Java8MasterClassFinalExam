bfed716c261edd76ac8a1b62794b4e3b1665d36d
Aaron Macandili <aaron.macandili@cognizant.com>
2023-06-26 15:49:31 +0800

    First commit

---
+++ src/test/java/com/amacandili/java8masterclass/finalexam/FinalexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amacandili.java8masterclass.finalexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
